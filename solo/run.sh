ca89f7144b14bb81292127dbbd95d8a4ac3f87d6
mufengcoding <mufeng5619@gamil.com>
2019-03-06 03:49:09 +0800

    修正

--- solo/run.sh
+++ solo/run.sh
@@ -1,1 +1,4 @@
-cd /root/blog && java -cp WEB-INF/lib/*:WEB-INF/classes org.b3log.solo.Starter
+#!/bin/bash
+source /etc/profile
+cd /root/blog 
+java -cp WEB-INF/lib/*:WEB-INF/classes org.b3log.solo.Starter
