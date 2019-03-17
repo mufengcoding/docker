c67de52fb89e3abeff4ead2cf3fb28401d7e97fd
mufengcoding <mufeng5619@gamil.com>
2019-03-17 23:57:41 +0800

    auto 测试

--- autoupdate/autoupdte.py
+++ autoupdate/autoupdte.py
@@ -9,7 +9,7 @@ api_url = 'https://api.github.com/repos/b3log/solo/releases/latest'
 r = requests.get(api_url)
 a = json.dumps(r.json())
 b = json.loads(a)
-# print a
+
 print b['name']
 f = open('/root/docker/solo/Dockerfile', 'r')
 content = f.read()
@@ -17,14 +17,14 @@ f.close()
 v = open('/root/docker/autoupdate/version', 'r')
 vcontent = v.read()
 v.close()
-t = content.replace(vcontent,b['name'])
-#t = content.replace("3.3.0","3.1.0")
+#t = content.replace(vcontent,b['name'])
+t = content.replace("3.3.0","3.1.0")
 with open("/root/docker/solo/Dockerfile","w") as f2:
     f2.write(t)
 with open("/root/docker/autoupdate/version","w") as f1:
-    f1.write(vcontent)
-os.system('git tag -a '+b["name"])
-#os.system('git tag -a 3.1.0')
-os.system('git commit -a -m 123')
+    f1.write(b['name'])
+# os.system('git tag -a '+b["name"])
+os.system('git tag -a 3.1.0')
+os.system('git commit -a -m '+b['name'])
 os.system('git push')
 os.system('git push origin --tags')
