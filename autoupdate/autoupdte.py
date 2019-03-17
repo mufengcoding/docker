1ed6482f21a5a547a1e83b8c6a623b803a758641
mufengcoding <mufeng5619@gamil.com>
2019-03-17 23:53:43 +0800

    Merge branch 'master' of https://github.com/mufengcoding/docker
    # Conflicts:
    #	autoupdate/autoupdate.py

--- autoupdate/autoupdte.py
+++ autoupdate/autoupdte.py
@@ -14,12 +14,17 @@ print b['name']
 f = open('/root/docker/solo/Dockerfile', 'r')
 content = f.read()
 f.close()
-
-t = content.replace("3.2.0",b['name'])
-t = content.replace("3.3.0","3.1.0")
+v = open('/root/docker/autoupdate/version', 'r')
+vcontent = v.read()
+v.close()
+t = content.replace(vcontent,b['name'])
+#t = content.replace("3.3.0","3.1.0")
 with open("/root/docker/solo/Dockerfile","w") as f2:
     f2.write(t)
-#os.system('git tag -a '+b["name"])
-os.system('git tag -a 3.1.0')
+with open("/root/docker/autoupdate/version","w") as f1:
+    f1.write(vcontent)
+os.system('git tag -a '+b["name"])
+#os.system('git tag -a 3.1.0')
 os.system('git commit -a -m 123')
 os.system('git push')
+os.system('git push origin --tags')
